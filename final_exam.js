b3e08ea703eb3ec8ee1ef52d77e673891437a2ca
Bertalan Eva <eva0bertalan@gmail.com>
2016-03-16 11:04:59 +0100

    final version

--- final_exam.js
+++ final_exam.js
@@ -2,23 +2,24 @@
 
 var fs = require('fs');
 
-function ObjectToJson(fileName, object) {
+function ObjectToJson(fileName, object, fs) {
   this.fileName = fileName;
   this.object = object;
+  this.fs = fs;
 }
 
 ObjectToJson.prototype.writeObjectToJson = function(cb){
   var _this = this;
-  setTimeout(function() {
-    _this.fs.writeFile(_this.fileName, function(err, object){
+  setTimeout(function(err, object) {
       if (err) {
         return cb(err);
       };
-      cb(null, JSON.stringify(object));
-    });
+      cb(null, _this.fs.writeFile(_this.fileName, JSON.stringify(_this.object)));
   }, 2000);
 };
 
-var writeObject = new ObjectToJson('./exam.json', {'medve': 'majom'});
+var writeObject = new ObjectToJson('./exam.json', {'medve': 'majom'}, fs);
  
-writeObject.writeObjectToJson();
+writeObject.writeObjectToJson(function(err, object){
+  console.log(object);
+});
